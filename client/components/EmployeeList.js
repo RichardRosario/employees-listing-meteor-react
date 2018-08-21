63a20952ca9899737d428523aee564cde53061c5
unknown <tech2js@gmail.com>
2018-08-21 21:58:19 +0800

    added pagination to display 20 list and add more button

--- client/components/EmployeeList.js
+++ client/components/EmployeeList.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './EmployeeDetail';
 
 const PER_PAGE = 20;
 
-const EmployeeList = (props) => {
-  console.log(props.employees);
-  return (
-    <div>
-      <h1>Employee Lists</h1>
-      <div className="employee-list">
-        {props.employees.map(employee => <EmployeeDetail key={employee._id} employee={employee} />)}
+class EmployeeList extends Component {
+  componentWillMount() {
+    this.page = 1;
+  }
+
+  handleButtonClick() {
+    Meteor.subscribe('employees', PER_PAGE * (this.page + 1));
+    this.page += 1;
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h1>Employee Lists</h1>
+        <div className="employee-list">
+          {this.props.employees.map(employee => <EmployeeDetail key={employee._id} employee={employee} />)}
+        </div>
+        <button onClick={this.handleButtonClick.bind(this) } 
+          className="btn btn-primary">Load More...</button>
       </div>
-      <button onClick={() => console.log('clicked')} 
-        className="btn btn-primary">Load More...</button>
-    </div>
-  )
+    )
+  }
 }
 
 export default createContainer(() => {
